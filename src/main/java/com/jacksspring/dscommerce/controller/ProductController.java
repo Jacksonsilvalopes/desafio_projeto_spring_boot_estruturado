6c7361a6c65485d15cc1fe7abe87cc5309741bf1
Jackson Silva Lopes <j.a.ckson@hotmail.com>
2024-02-21 00:04:41 -0300

    implements access control on the Product controller

--- src/main/java/com/jacksspring/dscommerce/controller/ProductController.java
+++ src/main/java/com/jacksspring/dscommerce/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -38,6 +39,7 @@ public class ProductController {
     }
 
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 
     @PostMapping
     public ResponseEntity<ProductDTO> create(@Valid @RequestBody ProductDTO productDTO) {
@@ -50,6 +52,7 @@ public class ProductController {
 
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @PutMapping(value = "/{id}")
     public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO productDTO) {
 
@@ -58,6 +61,7 @@ public class ProductController {
 
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         productService.delete(id);
